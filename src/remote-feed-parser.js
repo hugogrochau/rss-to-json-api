f8b890add52e17262aa98d7ca7424e5460c47ca7
Hugo Grochau <hugo@grochau.com>
2017-03-25 19:55:20 -0300

    Search for images inside media objects

--- src/remote-feed-parser.js
+++ src/remote-feed-parser.js
@@ -1,5 +1,6 @@
 import fetch from 'isomorphic-fetch';
 import xml2js from 'xml2js';
+import get from 'lodash/get';
 
 const convert = (body) =>
   new Promise((resolve, reject) => {
@@ -20,6 +21,28 @@ const organize = (data) => {
   if (feed.image && feed.image.url) {
     feed.image = feed.image.url;
   }
+
+  // search for item images
+  items.forEach((item) => {
+    const mediaContent = get(item, 'media:content');
+    let imageUrl;
+    // if there are multple media:contents, grab the first
+    if (Array.isArray(mediaContent)) {
+      const mediaUrl = get(mediaContent, '[0].$.url');
+      imageUrl = mediaUrl || imageUrl;
+    // otherwise grab the only one
+    } else {
+      const mediaUrl = get(mediaContent, '$.url');
+      imageUrl = mediaUrl || imageUrl;
+    }
+    // if there's a media:thumbnail defined, prioritize it over all others
+    const mediaThumbnailUrl = get(mediaContent, 'media:thumbnail.$.url');
+    if (mediaThumbnailUrl) {
+      imageUrl = mediaThumbnailUrl || imageUrl;
+    }
+    item.image = imageUrl;
+  });
+
   return { feed, items };
 };
 
