a14c0d6ea957776bdec88a0f6fdf4db7c3da7ad7
Hugo Grochau <hugo@grochau.com>
2017-03-25 16:39:28 -0300

    Add initial express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Hello world');
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.server = http.createServer(app);
+
+app.use(bodyParser.json({
+  limit: '100kb',
+}));
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.get('/', (req, res) => {
+  res.send({ hello: 'world' });
+});
+
+app.listen(3000);
+
+console.log('Listening on port 3000');
